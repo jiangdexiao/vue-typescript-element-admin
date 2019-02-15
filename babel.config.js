cc89e48b30b2edc898f6bcd320e21695ce8f6fc9
jiangdexiao <jiangdexiao@icarbonx.com>
2019-02-15 18:48:31 +0800

    create project

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  presets: [
+    '@vue/app',
+    // ['env',            //添加 babel-preset-env 配置 // env项是借助插件babel-preset-env，下面这个配置说的是babel对es6,es7,es8进行转码，并且设置amd,commonjs这样的模块化文件，不进行转码
+    //   {
+    //     'modules': false
+    //   }
+    // ]
+  ],
+  plugins: [        // //需要npm install babel-plugin-component -D//官网：http://element-cn.eleme.io/#/zh-CN/component/quickstart
+    [
+      'component',
+      {
+        'libraryName': 'element-ui',  //按需引用element-ui插件
+        'styleLibraryName': 'theme-chalk' //按需引用element-ui主题
+      }
+    ]
+  ]
+}
---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-button>el-button</el-button>
+    </div>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import HelloWorld from './components/HelloWorld.vue'
+
+@Component({
+  components: {
+    HelloWorld
+  }
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
